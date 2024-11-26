56e8f8f4d1ceada7a701e801906bd8e68b965bc2
Chizaram Precious <chizaramprecious97@gmail.com>
2024-11-26 18:26:13 +0100

    usenavigate on frontend

--- frontend/src/pages/myProduct.tsx
+++ frontend/src/pages/myProduct.tsx
@@ -1,24 +1,29 @@
-import { useMutation } from "react-query"
+import { useMutation } from "react-query";
 import { toast } from "react-toastify";
-import { httpAddMyProduct } from "../api/AddProduct"
+import { httpAddMyProduct } from "../api/AddProduct";
 import AddProductForm from "../components/AddShopList";
+import { useNavigate } from "react-router-dom";
 
 const AddMyProduct = () => {
+    const navigate = useNavigate();
+
     const { mutate, isLoading } = useMutation(httpAddMyProduct, {
         onSuccess: () => {
             toast.success("Product Saved");
+            navigate("/shop");
         },
         onError: (error: Error) => {
             toast.error(error.message);
         },
     });
-    const handleSave = (productFormData: FormData) => {
 
-        mutate(productFormData)
-    }
+    const handleSave = (productFormData: FormData) => {
+        mutate(productFormData);
+    };
+
     return (
         <AddProductForm onSave={handleSave} isLoading={isLoading} />
-    )
-}
+    );
+};
 
-export default AddMyProduct
+export default AddMyProduct;
